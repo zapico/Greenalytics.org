6d890a1bf472632f773de8e472309fa2b1b7476a
Jorge Luis Zapico <zapico@zapico-mb.lan>
2009-11-11 23:24:26 +0100

    Test working

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,62 @@
+class SitesController < ApplicationController
+ require 'rubygems'
+ require 'rugalytics'
+ require 'universal_ruby_whois' 
+ require 'whois'
+ require 'garb'
+ require 'hpricot'
+ require 'open-uri'
+ require 'gattica'
+
+
+ 
+ def test
+    #Garb::Session.login('jorgezapico@gmail.com', 'rip2maQ1')
+    #Garb::Account.all.first
+    #@profile = Garb::Profile.all
+    
+    Rugalytics.login 'jorgezapico@gmail.com', 'rip2maQ1'
+    @profile = Rugalytics.find_profile('Verde media','carbon.to')
+    
+     today= DateTime.now-1.days
+     amonthago = today-30.days
+     today = today.strftime("%Y-%m-%d")
+     amonthago = amonthago.strftime("%Y-%m-%d")
+     gs = Gattica.new({:email => 'jorgezapico@gmail.com', :password => 'rip2maQ1', :profile_id => 21441200})
+     @results = gs.get({:start_date => amonthago, :end_date => today, :dimensions => 'pagePath', :metrics => 'pageviews', :sort => '-pageviews'}).to_s
+     
+    url = "http://carbon.to"
+    
+    # Get HTML Size
+    @length = open(url).length
+    
+    @hp = Hpricot(open(url))
+    
+    @pictures_size = 0
+    # Get images size
+    @hp.search("img").each do |p|
+      picurl = url+p.attributes['src']
+      @pictures_size += open(picurl).length
+    end
+     
+     @scripts = 0
+     # Get CSS size
+     @hp.search("link").each do |p|
+      cssurl = url+p.attributes['href']
+      @scripts += open(cssurl).length
+    end
+    # Get script size
+    @hp.search("html/head//script").each do |p|
+      scripturl = url+p.attributes['src']
+      @scripts += open(scripturl).length
+    end
+    
+
+    
+    @w = Whois::Client.new
+    @w.query('70.32.99.240')
+
+ 
+ end
+
+end
