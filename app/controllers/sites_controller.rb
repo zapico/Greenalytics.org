96083ea2bbd5dbd7d6ad33a296f9d9e5f29ddf5b
Jorge Luis Zapico <zapico@zapico-mb.lan>
2010-01-16 19:54:00 +0100

    Better design for test and total co2

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -15,21 +15,23 @@ class SitesController < ApplicationController
     
     Rugalytics.login 'jorgezapico@gmail.com', 'rip2maQ1'
     @profile = Rugalytics.find_profile('Verde media','carbon.to')
-    
+     
+     # GET DATA FROM GOOGLE ANALYTICS
      today= DateTime.now-1.days
      amonthago = today-30.days
      today = today.strftime("%Y-%m-%d")
      amonthago = amonthago.strftime("%Y-%m-%d")
      gs = Gattica.new({:email => 'jorgezapico@gmail.com', :password => 'rip2maQ1', :profile_id => 21441200})
-     @results = gs.get({:start_date => amonthago, :end_date => today, :dimensions => 'pagePath', :metrics => 'pageviews'})
+     # Get the pageview of all apges
+     allpages = gs.get({:start_date => amonthago, :end_date => today, :dimensions => 'pagePath', :metrics => 'pageviews'})
      # Get the time on site by country
      visitors = gs.get({:start_date => amonthago, :end_date => today, :dimensions => 'country', :metrics => 'timeOnSite', :aggregates => 'country'})
      
-   
+     # CALCULATE VISITORS IMPACT
      # Parse the time on site by country 
      @visitors_text = " "
      @time = 0 
-     @totalco2 = 0
+     @co2_visitors = 0
      visitors.points.each do |country|
        # Parse country
        name = country.to_s.split('ga:country=')[1]
@@ -54,7 +56,7 @@ class SitesController < ApplicationController
        
        # Calculate the impact
        carbonimpact = factor.to_f * time.to_i * 20 / 3600000
-       @totalco2 += carbonimpact
+       @co2_visitors += carbonimpact
        
        # Aggregate
        time = (time.to_f/60).round(1)
@@ -65,40 +67,83 @@ class SitesController < ApplicationController
        end
      end     
      
+     @co2_visitors = @co2_visitors/1000
      @time = @time/60
      
      @main_url = "http://carbon.to"
     
-    # Get HTML Size
-    @length = open(@main_url).length
-    
-    @hp = Hpricot(open(@main_url))
-    
-    @pictures_size = 0
-    # Get images size
-    @hp.search("img").each do |p|
-      picurl = @main_url+p.attributes['src']
-      @pictures_size += open(picurl).length
-    end
-     
-     @scripts = 0
-     # Get CSS size
-     @hp.search("link").each do |p|
-      cssurl = @main_url+p.attributes['href']
-      @scripts += open(cssurl).length
-    end
-    # Get script size
-    @hp.search("html/head//script").each do |p|
-      scripturl = @main_url+p.attributes['src']
-      @scripts += open(scripturl).length
-    end
-    
 
+    # CALCULATE TOTAL TRAFFIC
+    # Initialiate variables
+    @total_size = 0
+    @page_text = ""
+    # Iterate through the different pages
+    allpages.points.each do |point|
+    	# 1. Get the URL
+    	url = point.to_s.split("pagePath=>")[1]
+    	url = url.to_s.split("}")[0]
+    	url = url.to_s.split('"')[1]
+    	url = url.to_s.split('"')[0]
+    	# 2. Get the number of visitors
+    	visits = point.to_s.split("pageviews=>")[1]
+    	visits = visits.to_s.split("}")[0]
+    	# 3. Aggregate text
+    	pagesize = pageSize(@main_url+url)/1024
+    	@page_text += "<p><b>" + url  + "</b></p>"
+    	@page_text += "<p>" + pagesize.to_s + " kb. " +visits.to_s+ " visitors</p>"
+    	@total_size += pagesize*visits.to_i
+    end
+   
+    # CALCULATE SERVER AND INFRA IMPACT
+    @co2_server = 0
+    @co2_infrastructure = 0
+    @co2_server = @total_size*0.001*0.5017400
     
     @w = Whois::Client.new
     @w.query('70.32.99.240')
-
+    
+    # Aggregate total CO2
+    @total_co2= 0
+    @total_co2 = @co2_visitors + @co2_server
+ 
+    # TRANSLATE USING CARBON.TO
+    beer = Net::HTTP.get(URI.parse("http://carbon.to/beers.json?co2="+@total_co2.round.to_s))
+    beer = ActiveSupport::JSON.decode(beer)
+    @beeramount = beer["conversion"]["amount"]
+    car = Net::HTTP.get(URI.parse("http://carbon.to/car.json?co2="+@total_co2.round.to_s))
+    car = ActiveSupport::JSON.decode(car)
+    @caramount = car["conversion"]["amount"]
  
  end
+ 
+ # Gives back the page size of a URL
+ def pageSize (url)
+    # Get HTML Size
+   total = 0
+   begin
+   total = open(url).length
+   
+   hp = Hpricot(open(url))
+   
+   # Get images size
+   hp.search("img").each do |p|
+     picurl = url+p.attributes['src']
+     total += open(picurl).length
+   end
+    # Get CSS size
+    hp.search("link").each do |p|
+     cssurl = url+p.attributes['href']
+     total += open(cssurl).length
+   end
+   # Get script size
+     hp.search("html/head//script").each do |p|
+     scripturl = url+p.attributes['src']
+     total += open(scripturl).length
+   end
+   ensure
+   return total
+   end
+ end
+ 
 
 end
