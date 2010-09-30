5ecb7bf5f0d998a91ceca420e1c6b903b9d1be48
Jorge Zapico <zapico@kth.se>
2010-09-30 19:30:39 +0200

    added vendor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  include AuthenticatedSystem
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  # Be sure to include AuthenticationSystem in Application Controller instead
-  include AuthenticatedSystem
   require 'gdata'
 
   # Create new user
