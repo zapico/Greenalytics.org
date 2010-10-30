6a6c6ac9f719befeb9943279292f894948aae8c3
Jorge Zapico <zapico@kth.se>
2010-10-30 16:36:22 +0200

    removed round

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,10 +9,10 @@ module ApplicationHelper
   def formatco2(co2)
       co2 = co2.to_i
       if co2 > 1000000  
-          return "<co2> " + ((co2/1000000).round(0)).to_s + "</co2> tons CO<sub>2</sub>"  
+          return "<co2> " + ((co2/1000000).round).to_s + "</co2> tons CO<sub>2</sub>"  
        end  
        if co2 > 1000  
-          return "<co2> " + ((co2/1000).round(0)).to_s + "</co2> kg CO<sub>2</sub>"  
+          return "<co2> " + ((co2/1000).round).to_s + "</co2> kg CO<sub>2</sub>"  
        else 
          return "<co2> " + co2.to_s + "</co2> grams CO<sub>2</sub>" 
        end
@@ -21,7 +21,7 @@ module ApplicationHelper
  def formattime(mins)
    mins = mins.to_i
    if mins > 60
-     return ((mins/60).round(0)).to_s + " hours"
+     return ((mins/60).round).to_s + " hours"
    else
      return mins.to_s + " minutes"
    end
