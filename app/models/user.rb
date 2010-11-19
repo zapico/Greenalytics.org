5f7b33b4cc9c86cabb0293ad0a297d0ee94854ce
zapico <zapico_admin@susmac-02.nada.kth.se>
2010-11-19 11:56:04 +0100

    invitation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   include Authentication::ByPassword
   include Authentication::ByCookieToken
   has_many :sites
+  attr_accessor :invitation
   
   validates_presence_of     :login
   validates_length_of       :login,    :within => 3..40
@@ -19,7 +20,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
-  
+  validate_presence_of      :invitation
 
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
