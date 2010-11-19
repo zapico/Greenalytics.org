8944ab808ebc170bd14f1dec7b9baa02181218c2
zapico <zapico_admin@susmac-02.nada.kth.se>
2010-11-19 11:48:25 +0100

    added code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   
   def create
     logout_keeping_session!
+    if params[:user].invitation = "greenmywebsite"
     @user = User.new(params[:user])
     success = @user && @user.save
     if success && @user.errors.empty?
@@ -27,6 +28,10 @@ class UsersController < ApplicationController
       self.current_user = @user # !! now logged in
       redirect_back_or_default('/connect')
       flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+    else
+      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      render :action => 'new'
+    end
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
       render :action => 'new'
