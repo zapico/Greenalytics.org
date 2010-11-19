098afe385ebc2e98066ddeb8cd14255bc135bcf3
zapico <zapico_admin@susmac-02.nada.kth.se>
2010-11-19 11:57:20 +0100

    invitation

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
-  validate_presence_of      :invitation
+  validates_presence_of      :invitation
 
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
