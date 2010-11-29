377277666e22342fc242a6220ddcd9fdb6418b80
Jorge Zapico <zapico@kth.se>
2010-11-29 15:22:37 +0100

    Round

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,12 +7,12 @@ module ApplicationHelper
  end
  
   def formatco2(co2)
-      co2 = co2.to_i
+      co2 = co2.to_f
       if co2 > 1000000  
-          return "<co2> " + ((co2/1000000).to_f).to_s + "</co2> tons CO<sub>2</sub>"  
+          return "<co2> " + ((co2/1000000).round(1)).to_s + "</co2> tons CO<sub>2</sub>"  
        end  
        if co2 > 1000  
-          return "<co2> " + ((co2/1000).to_f).to_s + "</co2> kg CO<sub>2</sub>"  
+          return "<co2> " + ((co2/1000).round(1)).to_s + "</co2> kg CO<sub>2</sub>"  
        else 
          return "<co2> " + co2.to_s + "</co2> grams CO<sub>2</sub>" 
        end
